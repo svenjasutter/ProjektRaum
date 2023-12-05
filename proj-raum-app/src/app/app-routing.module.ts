9b788e3e2ceaaccd6e6a9409339363d2b1513d1f
Svenja Sutter <svenja.sutter2@gmail.com>
2023-12-05 14:07:54 +0100

    set home page to map

--- proj-raum-app/src/app/app-routing.module.ts
+++ proj-raum-app/src/app/app-routing.module.ts
@@ -6,8 +6,9 @@ import { NewsComponent } from './views/news/news.component';
 import { FeedbackComponent } from './views/feedback/feedback.component';
 import { ProjectsComponent } from './views/projects/projects.component';
 import { FeedbackProjectComponent } from './views/feedback-projects/feedback-projects.component';
+import { MapComponent } from './components/map/map.component';
 
-const routes: Routes = [{ path: 'home', component: HomeComponent }, { path: 'projects', component: ProjectsComponent }, { path: 'infos', component: InfosComponent }, { path: 'news', component: NewsComponent }, { path: 'feedback', component: FeedbackComponent }, { path: 'feedback-projects', component: FeedbackProjectComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'projects', component: ProjectsComponent }, { path: 'infos', component: InfosComponent }, { path: 'news', component: NewsComponent }, { path: 'feedback', component: FeedbackComponent }, { path: 'feedback-projects', component: FeedbackProjectComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
